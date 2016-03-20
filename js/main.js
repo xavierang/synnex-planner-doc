90ff81d806299c77987a0ea1db78f065ca303d6d
xavierang <xavier_ang@Xavier-Xian-Wu-ANGs-MacBook-Pro.local>
2016-03-20 12:55:33 +1100

    transitions and responsive

--- js/main.js
+++ js/main.js
@@ -34,20 +34,50 @@ $(document).ready(function(){
 		$('#dropdown-nav').addClass("hidden");
 	});
 
+	/*$('#sidebar-nav img').hover(function(){
+			$(this).attr('src','img/synnex-logo.png');
+		}, function(){
+			$(this).attr('src','img/synnex-logo-grayscale.png');
+		}
+	);*/
+
 	$('.read-btn').click(function(){
 		$('.read-more-text').css('display','inline');
 		$('.read-btn').addClass("hidden");
 	});
 
 	$('.members').click(function(){
-		$(this).next().toggleClass('hidden');
+		if($(this).hasClass('members-active')){
+			$(this).removeClass('members-active');
+			$(this).next().addClass('hidden');
+		}else{
+			//clear others
+			$('.members-detail').addClass('hidden');
+			$('.members').removeClass('members-active');
+
+			$(this).next().toggleClass('hidden');
+			$(this).toggleClass('members-active');
+		}
 	});
-	
+
 	$('#brands img').click(function(){
+		var brand = $(this).attr('alt');
+		if($(this).hasClass('brands-active')){
+			$(this).removeClass('brands-active');
+			$(this).attr('src','img/brands/'+brand+'-grayscale.jpg');
+			
+			//clear all opened accordion			
+			$(this).next().children('div').addClass('hidden');
+			$(this).next().children('h3').removeClass('accordion-active');
+		}else{
+			$(this).addClass('brands-active');
+			$(this).attr('src','img/brands/'+brand+'.jpg');
+		}
 		$(this).next().toggleClass('hidden');	
 	});
 
 	$('#brands h3').click(function(){
 		$(this).next().toggleClass('hidden');
+		$(this).toggleClass('accordion-active');
 	});
 });
